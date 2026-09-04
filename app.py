d2f02b82e6334ce7aa340020ce09e28f17cca90b
pj915054-cmd <pj915054@gmail.com>
2026-09-04 09:10:50 -0700

    i can recreate app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-a = 10
-b = 20
-print(a+b)
-print(a-b)
+from flask import flask,jsonify
+
+app = flask(__name__)
+
+@app.route("/")
+def home():
+  return jsonify({"message":"CI DEMO API is running"})
+  
+@app.health("/health")
+def health():
+  return jsonify({"status":"ok"})
+
+if __name__ == "__main__":
+  app.run(debug = True)
+  
