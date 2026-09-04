b1df239f9f16851acc8c13febe902c905a2066ba
pj915054-cmd <pj915054@gmail.com>
2026-09-04 09:19:09 -0700

    i recreate test_app.py

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+  app.config.update(TESTING=True)
+  return app.test_client()
+
+
+def test_home(client):
+  response = client.get("/")
+  assert response.status_code == 200
+  data = response.get_json()
+  assert data["message"] == "CI DEMO API is running"
+
+def test_health(client):
+  response = client.get("/health")
+  assert repsonse.status_code == 200
+  assert response.get_json() == {"status":"ok"}
